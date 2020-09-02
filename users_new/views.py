d87b88b644598909485051f53e7f112965789ebe
Sedef Bostancı <sedefbostanci.SB@gmail.com>
2020-09-02 15:19:09 +0300

    Update views.py

--- users_new/views.py
+++ users_new/views.py
@@ -103,13 +103,13 @@ class AuthViewSet(viewsets.GenericViewSet):
         obj = self.request.user
         return obj
 
-@api_view(['POST',])
+@api_view(['GET',])
 @permission_classes([AllowAny])
 
 def get_all(request,pk):
 
     cursor = connection.cursor()
-    print("şgdglkşl")
+
     try:
           cursor.execute("SELECT ud.device_Name,ds.* FROM users_new_customuser as u ,userDevices_userdevices as ud ,devicePlants_deviceplants as dp, deviceSlots_deviceslots as ds WHERE u.id= ud.user_id AND ud.id = dp.uDevice_ID_id AND dp.id = ds.devicePlants_ID_id AND u.id = %s",[pk])
 
